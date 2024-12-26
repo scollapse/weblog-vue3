a9d408b96cdcc30269db555fa93dec53583e7db3
syl <babaxiteyo@163.com>
2024-12-26 10:02:28 +0800

    添加 @vueuse/core 库，更新依赖并在 Header 组件中实现全屏切换功能

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -19,6 +19,14 @@
 
         <!-- 右侧用户菜单 -->
         <div class="relative flex items-center">
+            <button class="text-white focus:outline-none hover:text-cyan-500 mr-4" @click="toggle">
+                <svg v-if="!isFullscreen" xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M4 4h6v2H6v4H4V4zm10 0h6v6h-2V6h-4V4zm6 10v6h-6v-2h4v-4h2zm-10 6H4v-6h2v4h4v2z" />
+                </svg>
+                <svg v-else xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M18 18h-6v-2h4v-4h2v6zm-10 0H4v-6h2v4h4v2zm10-10V4h-6v2h4v4h2zm-10 0H4V4h6v2H6v4H4z" />
+                </svg>
+            </button>
             <button class="focus:outline-none flex items-center space-x-2 hover:bg-cyan-800 px-3 py-1 rounded-lg"
                 @click="toggleDropdown">
                 <img class="w-8 h-8 rounded-full border-2 border-cyan-500" src="https://via.placeholder.com/150"
@@ -47,6 +55,11 @@
 <script setup>
 import { ref } from 'vue';
 import { useMenuStore } from '@/stores/menu';
+
+import { useFullscreen } from '@vueuse/core';
+
+// isFullscreen 表示当前是否处于全屏；toggle 用于动态切换全屏、非全屏
+const { isFullscreen, toggle } = useFullscreen()
 
 const menuStore = useMenuStore();
 
